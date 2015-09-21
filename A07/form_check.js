967db06f0bcde109c301969af9d3207ba5385a99
Amy <amyli@hawaii.edu>
2015-09-20 18:08:13 -1000

    A07

--- A07/form_check.js
+++ A07/form_check.js
@@ -1,3 +1,14 @@
 /**
  * Created by Amy on 9/19/2015.
  */
+function validateForm() {
+  var x = document.getElementById("username").value;
+  var message = document.getElementById("message");
+  message.innerHTML = "";
+  try {
+    if (x == "") throw "Please input a username.";
+  }
+  catch (err) {
+    message.innerHTML = "Please input a username.";
+  }
+}
