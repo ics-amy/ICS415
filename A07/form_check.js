3c7f652266dcb9480a6cfa709b76b15ec486921d
Amy <amyli@hawaii.edu>
2015-09-21 01:01:50 -1000

    A07

--- A07/form_check.js
+++ A07/form_check.js
@@ -3,12 +3,25 @@
  */
 function validateForm() {
   var x = document.getElementById("username").value;
-  var message = document.getElementById("message");
-  message.innerHTML = "";
-  try {
-    if (x == "") throw "Please input a username.";
+  var y = document.getElementById("email").value;
+  var w = document.getElementById("password").value;
+  var z = document.getElementById("confirm").value;
+  if (x.length == "" || y.length == "") {
+    document.getElementById("username").style.borderColor = "red";
+    document.getElementById("email").style.borderColor = "red";
+    document.getElementById("e_username").innerHTML = "Username is empty.";
+    document.getElementById("e_email").innerHTML = "Email is empty.";
   }
-  catch (err) {
-    message.innerHTML = "Please input a username.";
+  if (w.length == "" || z.length == "") {
+    document.getElementById("password").style.borderColor = "red";
+    document.getElementById("confirm").style.borderColor = "red";
+    document.getElementById("e_password").innerHTML = "Password is empty.";
+    document.getElementById("e_confirm").innerHTML = "Confirm Password is empty.";
   }
+  if (w != z) {
+    document.getElementById("password").style.borderColor = "red";
+    document.getElementById("confirm").style.borderColor = "red";
+    document.getElementById("e_password").innerHTML = "Password do not match.";
+  }
+  return false;
 }
--- A07/myScript.js
+++ A07/myScript.js
@@ -1,3 +1,12 @@
 /**
  * Created by Amy on 9/19/2015.
  */
+function getClasses(elem) {
+  var classes = [];
+  document.getElementsByTagName("div").getAttribute("elem");
+  return classes;
+}
+
+function addClass(elem, className) {
+  document.getElementsByTagName("elem").setAttribute("elem","className");
+}
