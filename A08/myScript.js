ff273ecf21787aea2b103670acb120e4c52699a0
Amy Li <amyli@dhcp-168-105-86-83.wireless.manoa.hawaii.edu>
2015-09-21 14:34:59 -1000

    A08

--- A08/myScript.js
+++ A08/myScript.js
@@ -2,5 +2,8 @@
  * Created by amyli on 9/21/15.
  */
 $(document).ready(function() {
-  $("h3").each(function())
+  $(".answer").hide();
+  $(".question").click(function() {
+    $(".answer").slideToggle();
+  });
 });
