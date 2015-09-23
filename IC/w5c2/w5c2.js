163e871ac1d6c738691735411abb28e8126a3cc6
Amy Li <amyli@dhcp-168-105-80-83.wireless.manoa.hawaii.edu>
2015-09-23 10:02:03 -1000

    w5c2

---
+++ IC/w5c2/w5c2.js
@@ -0,0 +1,17 @@
+/**
+ * Created by amyli on 9/23/15.
+ */
+
+$(document).ready(function () {
+  $("button").click(function() {
+    $(".form-group").animate ({
+      height: "toggle"
+    });
+  });
+});
+
+$(document).ready(function() {
+  $("button").click(function() {
+    $(".container").load("tumblr_mwksi3W1fz1rqgx3ko1_500.jpg");
+  });
+});
